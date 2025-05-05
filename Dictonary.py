7cdea46e174b99066ce693791425359dfb43d560
rotoncsedu <roton1785@gmail.com>
2025-05-05 11:06:21 +0600

    Print Function in Python

---
+++ Dictonary.py
@@ -0,0 +1,10 @@
+name = 'Imran'
+age = 35
+print("My name is", name, "and I am",age ,"years old")
+print(f"My name is {name} and I am {age} years old.")
+print(f"My name is {name} and I am {age} years old.",end='****')
+print(name,age)
+print(name,age,sep="-")
+
+
+
