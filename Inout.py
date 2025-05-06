f8da158a20ee18c3962bfcbbd4051eab7b339b4e
rotoncsedu <roton1785@gmail.com>
2025-05-07 01:13:15 +0600

    While in List and Dictonary

--- Inout.py
+++ Inout.py
@@ -15,6 +15,8 @@ if num % 10 == 0:
 else:
     print(f"{num} is not multiple of 10")
 """
+import collections
+
 #While loop
 '''
 promt = "Enter the item you want to add as topping on your pizza : "
@@ -24,7 +26,7 @@ while  item != 'quit':
     item = input(promt)
     topping += item
 print(topping)
-'''
+
 while True:
     ticket_price = 0
     flag = 0
@@ -45,3 +47,52 @@ while True:
         else:
             ticket_price = 15
     print(f"Price for your ticket is : {ticket_price}$")
+
+unconfirmed_user = ['Meraj', 'Milon', 'Rahim']
+confirmed_user = []
+while unconfirmed_user:
+    current_user = unconfirmed_user.pop()
+    print(f'Verifying user: {current_user}')
+    confirmed_user.append(current_user)
+
+print('\n The following users have been confirmed: ')
+for name in confirmed_user:
+    print(name)
+
+pets = ['dog','cat', 'dog','goldfish','cat','rabbit','cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
+
+responses = {}
+polling_choice = True
+while polling_choice:
+    name = input("What is your name: ")
+    response = input("Which mountain would you like to climb oneday: ")
+
+    responses[name] = response
+
+    repeat_choice = input("Would any other like to respond (y/n): ")
+    if repeat_choice == 'n':
+        polling_choice = False
+
+print("\n ********Poll Result********")
+for name, response in responses.items():
+    print(f"{name} would like to clim {response}")
+'''
+
+#Exercise
+sandwich_order = ['tuna','luna','funa']
+sandwich_made = []
+de = collections.deque(sandwich_order)
+while de:
+    current_order = de.popleft()
+    print(f"I made you {current_order} sandwich")
+    sandwich_made.append(current_order)
+
+
+print("\n ******** Finish Sandwich List ***********")
+for sandwich in sandwich_made:
+    print(sandwich)
+
